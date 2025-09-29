790aa59d4d8a88c5aef87bf1f3a259f0d7dac7aa
LauraLopez0110 <128760270+LauraLopez0110@users.noreply.github.com>
2025-09-29 15:49:05 -0500

    Nombre archivo

--- app/main.py
+++ app/main.py
@@ -1,13 +1,13 @@
 from app.database import Base, engine
 from app import models
 from fastapi import FastAPI
-from app.routers import clients, purchases
+from app.routers import client, purchases
 
 app = FastAPI(title="API VISE")
 Base.metadata.create_all(bind=engine)
 
 # Registrar los routers
-app.include_router(clients.router)
+app.include_router(client.router)
 app.include_router(purchases.router)
 @app.get("/")
 def read_root():
